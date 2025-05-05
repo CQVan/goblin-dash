6e756b9eb13d29393cfa9b1ba5086d2c252ce89a
Pranav1 <pranavjoshi1122@gmail.com>
2025-05-04 20:56:29 -0700

    finished community area lvl and added working key prefab
    drop off zones work with the key prefab

--- Assets/Scenes/Testing/Pranav_Scene/scripts/drop_off.cs
+++ Assets/Scenes/Testing/Pranav_Scene/scripts/drop_off.cs
@@ -6,10 +6,16 @@ public class drop_off : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private GameObject player;
+    private MeshRenderer rend;
+    private Collider col;
+    private Transform ps;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         text.enabled = false;
+        rend = GetComponent<MeshRenderer>();
+        col = GetComponent<Collider>();
+        ps = transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -23,6 +29,9 @@ public class drop_off : MonoBehaviour
         if (collision.gameObject == player)
         {
             StartCoroutine(showCompleted());
+            rend.enabled = false;
+            col.enabled = false;
+            ps.gameObject.SetActive(false);
         }
     }
 
@@ -31,5 +40,6 @@ public class drop_off : MonoBehaviour
         text.enabled=true;
         yield return new WaitForSeconds(5f);
         text.enabled=false;
+        Destroy(gameObject);
     }
 }
